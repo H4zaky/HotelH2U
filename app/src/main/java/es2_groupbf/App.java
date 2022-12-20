876ce7d8f63125feaf126194bb65f8eab6195e43
WallQ <sergio98@outlook.pt>
2022-12-20 21:29:56 +0000

    N/A

--- app/src/main/java/es2_groupbf/App.java
+++ app/src/main/java/es2_groupbf/App.java
@@ -1,21 +1,10 @@
 package es2_groupbf;
 
-import java.io.IOException;
 import java.util.List;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        OpenCSVReader csvReader = new OpenCSVReader();
-
-        try {
-            List<Client> list = OpenCSVReader.parseCSV();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+        List<Transaction> transactions = OpenCSV.parseCSV();
     }
 }
