7e5b22fdc9623f281f60a543b8e53172cab10285
WallQ <sergio98@outlook.pt>
2022-12-20 19:12:47 +0000

    Renamed class OpenCSVReader to OpenCSV

---
+++ app/src/main/java/es2_groupbf/OpenCSV.java
@@ -0,0 +1,55 @@
+package es2_groupbf;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class OpenCSV {
+    private File file;
+
+    public static List<Transaction> parseCSV() {
+        try {
+            File file = Util.getResourceFile("dataset-v2.csv");
+
+            if (Objects.isNull(file)) {
+                throw new Exception("Something went wrong!");
+            }
+
+            CSVReader csvReader = new CSVReader(new FileReader(file));
+
+            String[] columnMapping = {"ID", "Nationality", "Age", "DaysSinceCreation", "NameHash", "DocIDHash", "AverageLeadTime", "LodgingRevenue", "OtherRevenue", "BookingsCanceled", "BookingsCheckedIn", "PersonsNights", "RoomNights", "DaysSinceLastStay", "DaysSinceFirstStay", "DistributionChannel", "MarketSegment", "PurchaseDate", "PaymentMethod"};
+            List<Transaction> transactions = new ArrayList<>();
+
+            ColumnPositionMappingStrategy<Transaction> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Transaction.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Transaction> csvToBean = new CsvToBeanBuilder<Transaction>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .withSeparator(';')
+                    .build();
+
+            for (Transaction myTransaction : csvToBean) {
+                System.out.println(myTransaction.toString());
+                transactions.add(myTransaction);
+            }
+
+            csvReader.close();
+
+            return transactions;
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
