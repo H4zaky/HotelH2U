9c9fde3310ebb69e324345e6e9d61d9fa84ed836
WallQ <sergio98@outlook.pt>
2022-12-22 15:37:14 +0000

    Removed CSVReader and ColumnPositionMappingStrategy

--- app/src/main/java/es2_groupbf/OpenCSV.java
+++ app/src/main/java/es2_groupbf/OpenCSV.java
@@ -1,13 +1,11 @@
 package es2_groupbf;
 
-import com.opencsv.CSVReader;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,6 +14,8 @@ import java.util.Objects;
 public class OpenCSV {
 
     public static List<Transaction> parseCSV() {
+        List<Transaction> transactions = new ArrayList<>();
+
         try {
             File file = Util.getResourceFile("dataset-v2.csv");
 
@@ -23,17 +23,10 @@ public class OpenCSV {
                 throw new Exception("The application couldn't load the necessary data!");
             }
 
-            CSVReader csvReader = new CSVReader(new FileReader(file));
+            FileReader reader = new FileReader(file);
 
-            String[] columnMapping = {"ID", "Nationality", "Age", "DaysSinceCreation", "NameHash", "DocIDHash", "AverageLeadTime", "LodgingRevenue", "OtherRevenue", "BookingsCanceled", "BookingsCheckedIn", "PersonsNights", "RoomNights", "DaysSinceLastStay", "DaysSinceFirstStay", "DistributionChannel", "MarketSegment", "PurchaseDate", "PaymentMethod"};
-            List<Transaction> transactions = new ArrayList<>();
-
-            ColumnPositionMappingStrategy<Transaction> strategy = new ColumnPositionMappingStrategy<>();
-            strategy.setType(Transaction.class);
-            strategy.setColumnMapping(columnMapping);
-
-            CsvToBean<Transaction> csvToBean = new CsvToBeanBuilder<Transaction>(csvReader)
-                    .withMappingStrategy(strategy)
+            CsvToBean<Transaction> csvToBean = new CsvToBeanBuilder<Transaction>(reader)
+                    .withType(Transaction.class)
                     .withSeparator(';')
                     .build();
 
@@ -41,17 +34,16 @@ public class OpenCSV {
 
             while (myTransactionIterator.hasNext()) {
                 Transaction myTransaction = myTransactionIterator.next();
-                System.out.println(myTransaction.getId());
+                System.out.println(myTransaction.toString());
+                transactions.add(myTransaction);
             }
 
-            csvReader.close();
-
-            return transactions;
-        } catch (IOException exception) {
+        } catch (FileNotFoundException exception) {
             exception.printStackTrace();
-            return null;
         } catch (Exception exception) {
             throw new RuntimeException(exception);
         }
+
+        return transactions;
     }
 }
