1b7a3875938e5b05fab3afc744eb3a711fd94808
WallQ <sergio98@outlook.pt>
2022-12-22 15:36:50 +0000

    Init OpenCSV testing class

---
+++ app/src/test/java/es2_groupbf/OpenCSVTest.java
@@ -0,0 +1,14 @@
+package es2_groupbf;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+public class OpenCSVTest {
+    List<Transaction> transactions;
+
+    @BeforeEach
+    void parseCSV() {
+        
+    }
+}
