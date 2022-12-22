a373148a9a7e75ac313160eeae2e5af0f8e08e21
WallQ <sergio98@outlook.pt>
2022-12-22 15:37:56 +0000

    N/A

--- app/src/test/java/es2_groupbf/AppTest.java
+++ app/src/test/java/es2_groupbf/AppTest.java
@@ -1,11 +1,10 @@
 package es2_groupbf;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    @Test
+    public void appHasAGreeting() {
+        App app = new App();
     }
 }
