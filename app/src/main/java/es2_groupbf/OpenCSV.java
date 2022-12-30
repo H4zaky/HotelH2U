f09ecaa11531515de65413573698f3374a89d525
WallQ <sergio98@outlook.pt>
2022-12-30 16:08:19 +0000

    Added error handling for file not found

--- app/src/main/java/es2_groupbf/OpenCSV.java
+++ app/src/main/java/es2_groupbf/OpenCSV.java
@@ -8,34 +8,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class OpenCSV {
-    public static List<Transaction> parseCSV() {
+    public OpenCSV() {
+    }
+
+    public static List<Transaction> loadData(String filename) throws FileNotFoundException {
         List<Transaction> transactions = new ArrayList<>();
 
-        try {
-            File file = Util.getResourceFile("dataset.csv");
+        File file = Util.getFileFromResources(filename);
 
-            if (Objects.isNull(file)) {
-                throw new Exception("The application couldn't load the necessary data!");
-            }
+        FileReader reader = new FileReader(file);
 
-            FileReader reader = new FileReader(file);
+        CsvToBean<Transaction> csvToBean = new CsvToBeanBuilder<Transaction>(reader)
+                .withType(Transaction.class)
+                .withSeparator(';')
+                .build();
 
-            CsvToBean<Transaction> csvToBean = new CsvToBeanBuilder<Transaction>(reader)
-                    .withType(Transaction.class)
-                    .withSeparator(';')
-                    .build();
-
-            for (Transaction myTransaction : csvToBean) {
-                transactions.add(myTransaction);
-                System.out.println(myTransaction.toString());
-            }
-        } catch (FileNotFoundException exception) {
-            exception.printStackTrace();
-        } catch (Exception exception) {
-            throw new RuntimeException(exception);
+        for (Transaction myTransaction : csvToBean) {
+            transactions.add(myTransaction);
         }
 
         return transactions;
