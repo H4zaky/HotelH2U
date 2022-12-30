8746b7669c678ae1263fd78c419a08c26687bdc1
WallQ <sergio98@outlook.pt>
2022-12-30 16:04:28 +0000

    Unit tests for Util Class

--- app/src/test/java/es2_groupbf/UtilTest.java
+++ app/src/test/java/es2_groupbf/UtilTest.java
@@ -1,4 +1,21 @@
 package es2_groupbf;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 public class UtilTest {
+    @Test
+    @DisplayName("Getting file from resources with string as input being valid filename")
+    public void gettingFileWithStringAsInputValidFileName() {
+        Assertions.assertDoesNotThrow(() -> Util.getFileFromResources("dataset.csv"), "This test should return a file located in the resources folder");
+    }
+
+    @Test
+    @DisplayName("Getting file from resources with string as input being invalid filename")
+    public void gettingFileWithStringAsInputInvalidFileName() {
+        Assertions.assertThrows(FileNotFoundException.class, () -> Util.getFileFromResources("none.csv"), "This test should return a file located in the resources folder");
+    }
 }
