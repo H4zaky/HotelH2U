891716e78fd363fe3c2b6d26a6ec09684eadc3e7
WallQ <sergio98@outlook.pt>
2022-12-30 16:06:54 +0000

    Added loading data, created hashmap for mapping the transactions for clients and added menu

--- app/src/main/java/es2_groupbf/App.java
+++ app/src/main/java/es2_groupbf/App.java
@@ -1,10 +1,24 @@
 package es2_groupbf;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Transaction> transactions = OpenCSV.parseCSV();
+        List<Transaction> transactions = new ArrayList<>();
+        try {
+            transactions = OpenCSV.loadData("dataset.csv");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        HashMap<String, List<Transaction>> clients = new HashMap<>();
+        for (Transaction transaction : transactions) {
+            clients.putIfAbsent(transaction.getDocIdHash(), new ArrayList<>());
+            clients.get(transaction.getDocIdHash()).add(transaction);
+        }
+
         Menu menu = new Menu();
         menu.run();
     }
