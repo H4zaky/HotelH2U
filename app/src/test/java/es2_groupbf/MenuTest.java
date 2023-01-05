d7e7e8b7cb9eff43ba6c6d0dc8ff58193e156275
WallQ <sergio98@outlook.pt>
2023-01-05 17:33:05 +0000

    Refactored some tests and added some more basic tests

--- app/src/test/java/es2_groupbf/MenuTest.java
+++ app/src/test/java/es2_groupbf/MenuTest.java
@@ -14,34 +14,112 @@ public class MenuTest {
     }
 
     @Test
-    @DisplayName("Setting option value as integer being valid")
-    public void setOption() {
+    @DisplayName("Test getOption method with value 1")
+    void testGet_Option1() {
         menu.setOption(1);
-        Assertions.assertEquals(1, menu.getOption(), "This test should assign 1 to option class property");
+        Assertions.assertEquals(1, menu.getOption());
     }
 
     @Test
-    @DisplayName("Setting exit value as boolean being valid")
-    public void setExit() {
+    @DisplayName("Test getOption method with value 2")
+    void testGet_Option2() {
+        menu.setOption(2);
+        Assertions.assertEquals(2, menu.getOption());
+    }
+
+    @Test
+    @DisplayName("Test getOption method with value 3")
+    void testGet_Option3() {
+        menu.setOption(3);
+        Assertions.assertEquals(3, menu.getOption());
+    }
+
+    @Test
+    @DisplayName("Test setOption method with value 1")
+    void testSet_Option1() {
+        menu.setOption(1);
+        Assertions.assertEquals(1, menu.getOption());
+    }
+
+    @Test
+    @DisplayName("Test setOption method with value 2")
+    void testSet_Option2() {
+        menu.setOption(2);
+        Assertions.assertEquals(2, menu.getOption());
+    }
+
+    @Test
+    @DisplayName("Test setOption method with value 3")
+    void testSet_Option3() {
+        menu.setOption(3);
+        Assertions.assertEquals(3, menu.getOption());
+    }
+
+    @Test
+    @DisplayName("Test isExit method with value true")
+    void testIsExit_True() {
         menu.setExit(true);
-        Assertions.assertTrue(menu.isExit(), "This test should assign true to exit class property");
+        Assertions.assertTrue(menu.isExit());
     }
 
     @Test
-    @DisplayName("Getting option value as integer being valid")
-    public void getOption() {
-        Assertions.assertEquals(0, menu.getOption(), "This test should get 0 to option class property");
+    @DisplayName("Test isExit method with value true")
+    void testIsExit_False() {
+        menu.setExit(false);
+        Assertions.assertFalse(menu.isExit());
     }
 
     @Test
-    @DisplayName("Getting exit value as boolean being valid")
-    public void getExit() {
-        Assertions.assertFalse(menu.isExit(), "This test should get false to exit class property");
+    @DisplayName("Test setExit method with value true")
+    void testSetExit_True() {
+        menu.setExit(true);
+        Assertions.assertTrue(menu.isExit());
     }
 
     @Test
-    @DisplayName("Verify state of class Menu")
-    public void menuNotNull() {
-        Assertions.assertNotNull(menu, "This test should get not null for class Menu");
+    @DisplayName("Test setExit method with value false")
+    void testSetExit_False() {
+        menu.setExit(false);
+        Assertions.assertFalse(menu.isExit());
+    }
+
+    @Test
+    @DisplayName("Test performAction method with option 0")
+    void testPerformAction_Option0() {
+        menu.setOption(0);
+        menu.performAction();
+        Assertions.assertTrue(menu.isExit());
+    }
+
+    @Test
+    @DisplayName("Test performAction method with option 1")
+    void testPerformAction_Option1() {
+        menu.setOption(1);
+        menu.performAction();
+        Assertions.assertFalse(menu.isExit());
+    }
+
+    @Test
+    @DisplayName("Test performAction method with option 2")
+    void testPerformAction_Option2() {
+        menu.setOption(2);
+        menu.performAction();
+        Assertions.assertFalse(menu.isExit());
+    }
+
+    @Test
+    @DisplayName("Test performAction method with option 3")
+    void testPerformAction_Option3() {
+        menu.setOption(3);
+        menu.performAction();
+        Assertions.assertFalse(menu.isExit());
+    }
+
+    @Test
+    @DisplayName("Test performAction method with invalid option")
+    void testPerformAction_InvalidOption() {
+        menu.setOption(-1);
+        menu.performAction();
+        Assertions.assertFalse(menu.isExit());
     }
 }
